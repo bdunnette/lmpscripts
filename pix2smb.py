227c3d1c62a688cabd690a69b8ac3e851f1b8f6b
Brian Dunnette <dunn0172@umn.edu>
2013-06-28 13:35:12 -0500

    adding basic script to add EXIF tags to course images

--- pix2smb.py
+++ pix2smb.py
@@ -11,14 +11,19 @@ parser.add_argument('--dest_dir', help='directory to copy images to', default='\
 parser.add_argument('--move_to', help='directory to move copied files to')
 #parser.add_argument('--dest_dir', help='directory to copy images to', default='C:\\temp')
 parser.add_argument('--type', help='type of images being transferred', default='EM')
+parser.add_argument('--facility', help='facility where images were taken', default='UMMCF')
 parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
 args = parser.parse_args()
 #print args
 ctime = date.fromtimestamp(os.path.getctime(args.source_dir))
-for case in args.cases:
-	case = case.upper()
+for case_id in args.cases:
+	case_id = case_id.upper()
+	if args.type.capitalize() == 'Grosslab':
+		destination = '\\'.join([args.dest_dir, args.type, str(ctime.year), args.facility, case_id])
+	else:
+		destination = '\\'.join([args.dest_dir, args.type, str(ctime.year), case_id])
 	if args.verbose:
-		print "Copying files for case %s" % case
-	shutil.copytree(args.source_dir, '\\'.join([args.dest_dir, args.type, str(ctime.year), case]))
+		print "Copying files for case %s to %s" % (case_id, destination)
+	shutil.copytree(args.source_dir, destination)
 	if args.move_to:
-		shutil.move(args.source_dir, '\\'.join([args.move_to, case]))
+		shutil.move(args.source_dir, '\\'.join([args.move_to, case_id]))
