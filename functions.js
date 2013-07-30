2fada1fd5f90f4f9209eda33cdd3944591fbdf86
Brian D <brian@dunnette.us>
2013-07-29 20:09:10 -0500

    tweaking CSV output

--- functions.js
+++ functions.js
@@ -2,18 +2,19 @@ collections = [summer, cardio, resp, neuro, bjs, endo, repro, renal, gi, heme];
 //console.log(collections);
 for (collection in collections) {
 	collection_labs = collections[collection];
-	console.log('Labs: ', collection_labs);
+	//console.log('Labs: ', collection_labs);
 	for (lab in collection_labs) {
 		lab_path = collection_labs[lab][1];
 		lab_cases = collection_labs[lab][3];
-		console.log('Cases: ', lab_cases);
+		//console.log('Cases: ', lab_cases);
 		for (my_case in lab_cases) {
 			case_data = lab_cases[my_case];
-			case_title = case_data[1];
-			document.write([lab_path, case_title].join(), '<br/>');
+			console.log(case_data);
+			case_title = case_data[1].toString();
 			case_resources = case_data[2];
 			for (resource in case_resources) {
-				//console.log(case_resources[resource]);
+				resource_data = case_resources[resource];
+				document.write([['http://160.94.51.142/meded', lab_path, resource_data[0]].join('/'), JSON.stringify(case_title), JSON.stringify(resource_data[1])].join(), '<br/>');
 			};
 		};
 	};
